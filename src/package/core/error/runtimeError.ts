88790ccf2fd994451f238ac2ecdbf9132740a7bd
l-freeze <mmmm.yamamoto@gmail.com>
2024-06-12 23:33:41 +0900

    基底のエラーのjsdocを追加

--- src/package/core/error/runtimeError.ts
+++ src/package/core/error/runtimeError.ts
@@ -1,5 +1,8 @@
 import { RootError } from "./rootError";
 
+/**
+ * @summary 実行時エラーの基底
+ */
 export class RuntimeError extends RootError {
     constructor(message = "", code: number = Number.MAX_SAFE_INTEGER) {
         super(message);
